4d455019f1439eba8c34da33016d9fa68adceb76
Andres Rivero <andres19rivero@gmail.com>
2021-06-29 23:49:50 -0400

    Finish the functionality of the calculator

--- script.js
+++ script.js
@@ -1,15 +1,63 @@
-function add(){
+let numb1;
+let numb2;
+let operator;
+let response;
+const result = document.querySelector(".res");
+const buttonNumb = document.querySelectorAll(".number");
+const buttonClear = document.querySelector(".clear");
+const buttonOperator = document.querySelectorAll(".operator");
+const buttonEquals = document.querySelector(".equals");
 
+buttonNumb.forEach((button) => {
+  button.addEventListener("click", () => {
+    result.textContent += button.textContent;
+  });
+});
+buttonClear.addEventListener("click", () => {
+  result.textContent = "";
+});
+buttonOperator.forEach((buttonOp) => {
+  buttonOp.addEventListener("click", () => {
+    num1 = parseInt(result.textContent);
+    result.textContent = "";
+    operator = buttonOp.textContent;
+  });
+});
+buttonEquals.addEventListener("click", () => {
+  num2 = parseInt(result.textContent);
+  result.textContent = "";
+  operate(operator, num1, num2);
+});
+
+function add(number1, number2) {
+	response = number1 + number2
+	result.textContent = response
 }
-function subtract(){
-
+function subtract(number1, number2) {
+	response = number1 - number2
+	result.textContent = response
 }
-function multiply(){
-
+function multiply(number1, number2) {
+	response = number1 * number2
+	result.textContent = response
 }
-function divide(){
-
+function divide(number1, number2) {
+	response = number1 / number2
+	result.textContent = response
 }
-function operate(operator, number1, number2){
-
+function operate(operator, number1, number2) {
+  switch (operator) {
+    case "+":
+      add(number1, number2);
+      break;
+    case "-":
+      subtract(number1, number2);
+      break;
+    case "x":
+      multiply(number1, number2);
+      break;
+    case "/":
+      divide(number1, number2);
+      break;
+  }
 }
